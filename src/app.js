c1c7fe46dbb7cc556342ea8e603da5b68f673544
DevCrown333 <dev.tianlong33@gmail.com>
2024-07-18 15:42:11 +0300

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,24 @@
+import React, { Suspense } from 'react';
+import { createGlobalStyle } from 'styled-components';
+import '../public/css/Footer-Dark.css';
+import '../public/css/Navigation-Clean.css';
+import '../public/css/styles.css';
+import '../public/bootstrap/css/bootstrap.min.css';
+import '../public/fonts/fontawesome-all.min.css';
+import '../public/bootstrap/js/bootstrap.min.js';
+import { Presale } from './container/presale';
+// Import assets
+import 'modern-normalize/modern-normalize.css';
+
+// Import Components
+
+// Main page
+const App = () => {
+  return (
+    // <Suspense fallback={<div>Loading...</div>}>
+    <Presale />
+    // </Suspense>
+  );
+};
+
+export default App;
